3f522a07e3e155da4ae6eb32ac86e996e6d61054
Tim <81175852+wharvex@users.noreply.github.com>
2024-08-19 10:29:11 -0400

    first commit

---
+++ LAAUI.StartInterpreter/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace LAAUI.StartInterpreter
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
